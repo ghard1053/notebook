f55d944f56a3024790b79fc0c6e365723b58ba04
Takao <sukisya70@gmail.com>
2018-06-20 21:35:22 +0900

    change serializers

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,5 +1,17 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :birthdate
+  attributes :id, :name, :email, :birthdate #, :author
+
+  belongs_to :kind
+  has_many :phones
+  has_one  :address
+
+  # def author
+  #   "Jackson"
+  # end
+
+  meta do
+    { author: "Jackson" }
+  end
 
   def attributes(*args)
     h = super(*args)
